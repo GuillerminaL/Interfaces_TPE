4e0bb3a78da87499645f45f2e0f705b8b32eba9d
Guillermina Lauge <guillerminalauge@gmail.com>
2022-11-25 01:05:30 -0300

    Cards appearing and disappearing from right while scrolling

--- 4ta entrega/js/release_script.js
+++ 4ta entrega/js/release_script.js
@@ -1,24 +1,42 @@
 "use strict";
 
 //------------------ Cards animation from right -------------------
+/**
+ * Animate scroll: fija un intervalo para regular la aparición y animación de las cards, una a una. 
+ *                 Es llamada por el observador cuando la mitad de la sección entró en el viewport
+ * 
+ * @param {*} entries los elementos observados por el observador
+ */
+
+let cards = [];
 function animateScroll (entries) {
-    if(entries[0].isIntersecting) {
-        let cards = animatedContainer.children;
-        console.log(cards);
+    if(entries[0].isIntersecting) {      
+        cards = animatedContainer.children;
         let i = 0;
         let card = cards[i];
         let interval = setInterval(() => {
-            card.classList.add("scrolling");
+            card.classList.remove("disappearing");
+            card.classList.add("appearing");
             card = cards[i++];
             if(card == null) clearInterval(interval);
-        }, 700);
+        }, 700);  
+    } else {
+        if(cards.length > 0) {
+            let i = cards.length-1;
+            let card = cards[i];
+            let interval = setInterval(() => {
+                card.classList.add("disappearing");
+                card = cards[i--];
+                if(card == null) clearInterval(interval);
+            }, 300);
+        }
     }
 }
 
 let options = {
     root: null, //todo el viewport
     rootMargin: '0px', //por defecto, negativo dentro del vp | positivo fuera del vp
-    threshold: 0.1 //cuándo se ejecuta el código, de 0.0 a 1.0 (ej. mitad del obj 0.5)
+    threshold: 0.5 //cuándo se ejecuta el código, de 0.0 a 1.0 (ej. mitad del obj 0.5)
 }
 
 const observer = new IntersectionObserver(animateScroll, options);
