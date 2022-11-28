459a0ea1e1255bb6da2ffd0443ce5ad0345375a3
Guillermina Lauge <guillerminalauge@gmail.com>
2022-11-27 23:46:41 -0300

    Story columns scroll animation

--- 4ta entrega/js/release_script.js
+++ 4ta entrega/js/release_script.js
@@ -1,6 +1,16 @@
 "use strict";
 
 //------------------ Cards animation from right -------------------
+
+let options = {
+    root: null, //todo el viewport
+    rootMargin: '0px', //por defecto, negativo dentro del vp | positivo fuera del vp
+    threshold: 0.5 //cuándo se ejecuta el código, de 0.0 a 1.0 (ej. mitad del obj 0.5)
+}
+const cards = document.querySelectorAll(".cardLanzamiento");
+const animatedContainerObserver = new IntersectionObserver(animateScroll, options);
+const animatedContainer = document.querySelector(".cardsLanzamiento");
+
 /**
  * Animate scroll: fija un intervalo para regular la aparición y animación de las cards, una a una. 
  *                 Es llamada por el observador cuando la mitad de la sección entró en el viewport
@@ -17,9 +27,11 @@ function animateScroll (entries) {
             card.classList.remove("disappearing");
             card.classList.add("appearing");
             card = cards[i++];
-            if(card == null) clearInterval(interval);
+            if(card == null) {
+                clearInterval(interval);
+                cardsAreVisible = true;  
+            } 
         }, 500);
-        cardsAreVisible = true;  
     } else {
         if(cardsAreVisible) {
             let i = cards.length-1;
@@ -27,26 +39,24 @@ function animateScroll (entries) {
             let interval = setInterval(() => {
                 card.classList.add("disappearing");
                 card = cards[i--];
-                if(card == null) clearInterval(interval);
+                if(card == null) {
+                    clearInterval(interval);
+                    cardsAreVisible = false;
+                }
             }, 300);
-            cardsAreVisible = false;
         }
     }
 }
 
-let options = {
-    root: null, //todo el viewport
-    rootMargin: '0px', //por defecto, negativo dentro del vp | positivo fuera del vp
-    threshold: 0.5 //cuándo se ejecuta el código, de 0.0 a 1.0 (ej. mitad del obj 0.5)
-}
-
-const cards = document.querySelectorAll(".cardLanzamiento");
-const animatedContainerObserver = new IntersectionObserver(animateScroll, options);
-const animatedContainer = document.querySelector(".cardsLanzamiento");
 animatedContainerObserver.observe(animatedContainer);
 
 
 //------------------ Caracter carousel titles animation ---------------------
+const caractersCarObserver = new IntersectionObserver(animateTitles, options);
+const caractersCarousel = document.querySelector("#caracters-carousel");
+const caracterNames = document.querySelectorAll("div.personaje h2");
+const caracterAbilities = document.querySelectorAll("div.personaje p");
+
 let textsAreAnimated = false;
 function animateTitles (entries) {
     if(entries[0].isIntersecting) { 
@@ -66,9 +76,41 @@ function animateTitles (entries) {
     }
 }
 
-const caractersCarObserver = new IntersectionObserver(animateTitles, options);
-const caractersCarousel = document.querySelector("#caracters-carousel");
 caractersCarObserver.observe(caractersCarousel);
 
-const caracterNames = document.querySelectorAll("div.personaje h2");
-const caracterAbilities = document.querySelectorAll("div.personaje p");
+
+//------------------ Story columns scroll animation --------------------- 
+let imgRoute = '../images/lanzamiento/story/img';
+let imgType = '.jpg';
+const img = document.querySelector("div#story-img img");
+
+const storySection = document.querySelector("section.historia h2");
+const storyObserver = new IntersectionObserver(showStory, {root: null, rootMargin: '0px', threshold: 1});
+
+const paragraphs = document.querySelectorAll("#story-right-wrapper p");
+paragraphs.forEach((p) => {
+    const paragraphObserver = new IntersectionObserver(function(entries){
+        if(entries[0].isIntersecting) {  
+            img.src = imgRoute + p.getAttribute("data") + imgType;
+            if(img.classList.contains("invisible")) img.classList.remove("invisible");
+            if(p.classList.contains("invisible")) p.classList.remove("invisible");
+            img.classList.add("visible");
+            p.classList.add("visible");
+        } else {
+            if(p.classList.contains("visible")) p.classList.add("invisible");
+        }
+    }, {threshold: [1]});
+    paragraphObserver.observe(p);
+});
+
+function showStory(entries) {
+    if(entries[0].isIntersecting) { 
+        img.src = imgRoute + "0" + imgType;
+        if(img.classList.contains("invisible")) img.classList.remove("invisible");
+        if(storySection.classList.contains("invisible")) storySection.classList.remove("invisible");
+        img.classList.add("visible");
+        storySection.classList.add("visible");
+    }
+}
+storyObserver.observe(storySection);
+
